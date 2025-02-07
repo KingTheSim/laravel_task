16a94cb7f2c469bb06c0214029df9119fd37f2f7
KingTheSim <KingTheSim@abv.bg>
2025-02-07 21:40:42 +0200

    changing_tailwinds_a_bit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buttons Grid</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link
+      href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
 </head>
 <body class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="grid grid-cols-3 gap-4 w-3/4">
         @foreach ($buttons as $button)
             <a href="{{ $button->set ? $button->link : route('buttons.edit', $button->id) }}" 
-                class="flex items-center justify-center p-4 text-white rounded-lg shadow-md transition duration-300 transform hover:scale-105"
+                class="flex items-center justify-center  p-4 rounded-lg border-4 border-black relative"
                 style="background-color: {{ $button->color }}">
                     {{ $button->title }}
             </a>
